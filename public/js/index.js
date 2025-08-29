5a16db3f7844b0a00ae71f19fe77fcb7d24fc7fe
RubenDarioMolina <92568213+RubenDarioMolina@users.noreply.github.com>
2025-08-29 16:45:14 -0500

    Update index.js
    alert5

--- public/js/index.js
+++ public/js/index.js
@@ -73,8 +73,6 @@ if (bookBtn)
     bookTour(tourId);
   });
 const alertMessage = document.querySelector('body').dataset.alert;
-if (!alertMessage) {
-  showAlert('error','⚠️ No data-alert attribute found on <body>.');
+if (alertMessage) { 
+  showAlert('success', alertMessage);
 }
-console.log(alertMessage);
-if (alertMessage) showAlert('success', alertMessage);
